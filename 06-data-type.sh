9865c3b112b1bebccdbb0bed05f84a9560f4f0ea
k chakradhar <kchakradhar710@gmail.com>
2025-11-30 18:10:13 +0530

    hello world started

--- 06-data-type.sh
+++ 06-data-type.sh
@@ -4,7 +4,7 @@ NUMBER1=100
 NUMBER2=200
 
 TIMESTAMP=$(date)
-echo "scrpit executed at: $TIMSTAMP"
+echo "scrpit executed at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM OF $NUMBER1 AND $NUMBER2 is: $SUM"
