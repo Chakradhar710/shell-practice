861b37324817249b5d1d04b481e1f1992f3028aa
k chakradhar <kchakradhar710@gmail.com>
2025-12-01 18:08:25 +0530

    colors

--- 14-logs.sh
+++ 14-logs.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FLODER="/var/log/shellscript-logs"
-SCRIPT_NAME=$ (echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FLODER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FLODER
